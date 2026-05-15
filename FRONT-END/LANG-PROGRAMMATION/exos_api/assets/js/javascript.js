64c2e2ee97d049158193ea027dabdf11069b0566
Heriss <131191008+Mourad-Saidomar@users.noreply.github.com>
2026-05-15 15:25:32 +0300

    PC DWWM - 15:25 - 15-05-26

--- FRONT-END/LANG-PROGRAMMATION/exos_api/assets/js/javascript.js
+++ FRONT-END/LANG-PROGRAMMATION/exos_api/assets/js/javascript.js
@@ -46,23 +46,27 @@ fetch("https://jsonplaceholder.typicode.com/users")
 function afficherInfosCard(users) {
   users.forEach(user => {
     const carte = document.createElement('div');
-    carte.className = "hover-card card";
+    carte.className = "col";
     carte.innerHTML = `
-        <article class="p-3 bg-1 hover-card-inner text-white">
+        <article class="hover-card rounded-4 card h-100 border-0">
+            <div class="p-3 bg-1 rounded-4 hover-card-inner text-white h-100">
             <div class="card-body p-2">
-                <div class="text-center mb-3">
+                <div class="d-flex bg-blur-card p-3 rounded-pill align-items-center mb-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="80" height="90" fill="currentColor" class="bi bi-person-circle"  viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                     </svg>
+                    <p class="mb-2 ms-3 text-white col-5 overflow-hidden"><span class="text-white fw-bold fs-4">Bienvenue</span><br><strong class="d-block" title="${user.name}">${user.name}</strong></p>                    
                 </div>
-                <p class="mb-2"><strong>Name:</strong> ${user.name}</p>
-                <p class="mb-2"><strong>Username:</strong> ${user.username}</p>
-                <p class="mb-2"><strong>Email:</strong> ${user.email}</p>
-                <p class="mb-2"><strong>Phone:</strong> ${user.phone}</p>
-                <p class="mb-0"> <strong>Website:</strong>
-                    <a class="text-info text-decoration-none" href="${user.website}" target="_blank" rel="noopener noreferrer">hildegard.org</a>
-                </p>
+                <div class="row ms-2">
+                    <p class="mb-2 small text-white col-6 overflow-hidden"><span class="text-white text-decoration-underline">Email</span><br><strong class="d-block" title="${user.email}">${user.email}</strong></p>
+                    <p class="mb-2 small text-white col-6 overflow-hidden"><span class="text-white text-decoration-underline">Username</span><br><strong class="d-block" title="${user.username}">${user.username}</strong></p>
+                    <p class="mb-2 small text-white col-6 overflow-hidden"><span class="text-white text-decoration-underline">Phone</span><br><strong class="d-block" title="${user.phone}">${user.phone}</strong></p>
+                    <p class="mb-0 small text-white col-6 overflow-hidden"><span class="text-white text-decoration-underline">Website</span><br>
+                        <a href="https://${user.website}" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-white d-block" title="${user.website}">${user.website}</a>
+                    </p>
+                </div>
+            </div>
             </div>
         </article>
     `;
@@ -85,7 +89,7 @@ fetch("https://jsonplaceholder.typicode.com/users")
 changer.addEventListener("click", () => {
     table.classList.toggle("d-none");
     card.classList.toggle("d-none");
-})
+});
 
 
 
